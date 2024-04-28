40fbf112e4d11a2e145359da95fa9223a594e02b
polyhistor2050 <chrisspaul2050@gmail.com>
2024-04-28 23:20:18 +0300

    Fix saving and quiting neovim key bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,1 @@
- require("polyhistor")
+require("polyhistor")
--- nvim/lua/polyhistor/remap.lua
+++ nvim/lua/polyhistor/remap.lua
@@ -25,22 +25,20 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v")
 vim.keymap.set("n", "<leader>sh", "<C-w>s")
 
 -- Save changes
-vim.keymap.set("n", "<leader>w", "<cmd>w")
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 
 -- Quit NeoVim
-vim.keymap.set("n", "<leader>q", "<cmd>q")
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
 
 -- Save and quit NeoVim
-vim.keymap.set("n", "<leader>wq", "<cmd>wq")
+vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
 
 -- Quit NeoVim without saving changes
-vim.keymap.set("n", "<leader>qq", "<cmd>q!")
+vim.keymap.set("n", "<leader>qq", "<cmd>q!<CR>")
 
 -- nice highlight moves
 vim.keymap.set("v", "J", ":m '>+5<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
 
 -- Cursor jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -49,9 +47,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Keep the Cursor in the middle when searching
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
--- greatest remap ever
-
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
@@ -73,8 +68,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Make the file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 -- Highlight when yanking (copying) text
 -- Try it with `yap` in normal mode
